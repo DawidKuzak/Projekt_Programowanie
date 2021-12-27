0a7198810e80d7ccd166234c1b1694be3f2f0cf0
DawidKuzak <dawkuz@st.amu.edu.pl>
2021-12-27 21:31:29 +0100

    Obsługa eksportu tabeli do Jsona

--- Main.java
+++ Main.java
@@ -1,18 +1,26 @@
 import java.sql.*;
+import java.util.*;
+import java.io.FileWriter;
 import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Wybierz co chcesz zrobić:");
-        System.out.println("1. Eksportować dane do XML");
-        System.out.println("2. Importować dane z XML");
+        System.out.println("1. Eksportować dane do Json");
+        System.out.println("2. Importować dane z Json");
         System.out.println("3. Wpisać własną komendę");
         int choice = keyboard.nextInt();
-
+        String query;
+        Scanner scanner = new Scanner(System.in);
         switch(choice) {
+            case 1:
+                System.out.println("Jaką tabelę chcesz eksportować: ");
+                String dbname = scanner.nextLine();
+                query = "Select * from " + dbname;
+                Export export = new Export();
+                export.MySqlToJson(query);
             case 3:
-                Scanner scanner = new Scanner(System.in);
-                String query = scanner.nextLine();
+                query = scanner.nextLine();
                 Connector connector = new Connector();
                 connector.connect(query);
         }
@@ -42,6 +50,37 @@ class Connector {
             }
             con.close();
         } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class Export {
+    public void MySqlToJson(String query){
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://mysql.wmi.amu.edu.pl/s462090_Projekt_Programowanie", "s462090", "HasloTestowe123");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        FileWriter myWriter = new FileWriter("MySQLDB.json");
+        myWriter.write("{ \n");
+        while (rs.next()){
+            myWriter.write("{ \n");
+            for (int i=1; i<=columnsNumber; i++){
+                String columnValue = rs.getString(i);
+                String columnName = rsmd.getColumnName(i);
+                myWriter.write('"' + columnName + '"' + ": " + '"' + columnValue + '"' + ", \n");
+            }
+            myWriter.write("} \n");
+        }
+        myWriter.write("}");
+        myWriter.close();
+        con.close();
+        System.out.println("Wyeksportowano tabelę do MySQLDB.json");
+        }catch (Exception e) {
             System.out.println(e);
         }
     }
